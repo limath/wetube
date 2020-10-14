abdd98cfe3d9072fbdc8746b1e3f81258719ce79
Buyoun Lim <buyounlim@Buyouns-MacBook-Pro.local>
2020-10-14 23:52:37 +0900

    some comment added

--- controller/videoController.js
+++ controller/videoController.js
@@ -14,3 +14,6 @@ export const editVideo = (req, res) =>
   res.render("editVideo", { pageTitle: "Edit Video" });
 export const deleteVideo = (req, res) =>
   res.render("deleteVideo", { pageTitle: "Delete Video" });
+
+
+  //render의 두번쨰 변수로 템플릿마다 변수를 다르게 설정 
--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import routes from "./routes";
 
 export const localsMiddleware = (req, res, next) => {
-  res.locals.siteName = "WeTube";
+  res.locals.siteName = "WeTube"; //res.locals를 통해 변수를 템플릿에 globally 전달
   res.locals.routes = routes;
   next();
 };
